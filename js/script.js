b4cd37da14a06daf5f31d8715a453f3f2e7a742d
Adrian <color.ui>
2016-02-29 08:36:17 -0800

    animations and styles to experience section

--- js/script.js
+++ js/script.js
@@ -3,11 +3,22 @@ $(document).ready(function()
   $(window).scroll(function(event)
   {
     var y = $(this).scrollTop();
-    if (y >= 100)
+    if (y >= 700)
     {
-      $('.sec2').addClass('animate');
-      $('.sec3').addClass('animate');
-
+      $('.one').addClass('animate');
+      $('.two').addClass('animate2');
+      $('.three').addClass('animate3');
+      $('.four').addClass('animate4');
+      $('.five').addClass('animate5');
     }
+  });
+
+  $(window).scroll(function()
+  {
+    var top = $(window).scrollTop();
+    var number = -(top/4);
+    number = 'center '+number+'px';
+
+    $('.alien').css('background-position', number);
   });
 });
